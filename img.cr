7b2152c4f7f5db1eadd8a18f26dd783f52b64e35
Mario Arias <mario@marioarias.org>
2025-08-05 23:36:52 +0800

    Change Crystal implementation to be in pair with the other languages

--- img.cr
+++ img.cr
@@ -3,21 +3,41 @@ struct RGBPixel
 
   def initialize(@red : UInt8 = 0, @blue : UInt8 = 0, @green : UInt8 = 0, @alpha : UInt8 = 255)
   end
+
+  def set_red(r)
+    @red = r
+    self
+  end
+
+  def set_green(r)
+    @green = r
+    self
+  end
+
+  def set_blue(r)
+    @blue = r
+    self
+  end
 end
 
 def image_ramp_green(n)
   img = Array.new(n) { RGBPixel.new }
-  f = (255/(n - 1)).to_u8
-  (0...n).each { |i| img[i].green = (i * f).to_u8 }
+  f = (255/(n - 1))
+  # puts f
+  (0...n).each do |i|
+    img[i] = img[i].set_green((i.to_f * f).to_u8!)
+  end
   img
 end
 
 def image_to_gray(img, n)
   (0...n).each do |i|
-    y = (0.3 * img[i].red + 0.59 * img[i].green + 0.11 * img[i].blue).to_u8
-    img[i].red = y
-    img[i].green = y
-    img[i].blue = y
+    y = (0.3 * img[i].red + 0.59 * img[i].green + 0.11 * img[i].blue).to_u8!
+
+    imgi = img[i].set_red(y)
+    imgi = imgi.set_green(y)
+    img[i] = imgi.set_blue(y)
+    # puts img[i].green
   end
 end
 
